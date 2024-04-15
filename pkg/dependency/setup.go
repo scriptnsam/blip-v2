aa21d3e24d4cc7cf08ae3e4fca86016b85ef1f6d
ScriptnSam <oluwafemisam40@gmail.com>
2024-04-15 12:53:24 +0100

    Implemented cli setup for both macOS and linux OS and also implemented package install for both OS

--- pkg/dependency/setup.go
+++ pkg/dependency/setup.go
@@ -1,21 +1,37 @@
 package dependency
 
 import (
+	"runtime"
+
 	"github.com/scriptnsam/blip-v2/pkg/tools"
 )
 
 func SetupChocolatey() string {
-	// check if chocolatey has been installed
-	resp, err := tools.CheckChocolateyVersion()
-	if err == nil {
-		return resp
+	switch runtime.GOOS {
+	case "windows":
+		// check if chocolatey has been installed
+		resp, err := tools.CheckChocolateyVersion()
+		if err == nil {
+			return resp
+		}
+
+		// install chocolatey
+		if err := tools.InstallChocolatey(); err != nil {
+			return err.Error()
+		}
+
+		return "Chocolatey installed: Setup completed"
+	case "darwin":
+		// check if brew has been installed
+		r := tools.IsBrewInstalled()
+		if r {
+			return "Setup completed"
+		}
+		// retun install brew message
+		return `Brew not installed: please run the following command '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'`
+	case "linux":
+		return "Setup completed"
+	default:
+		return "Unsupported OS"
 	}
-
-	// install chocolatey
-	if err := tools.InstallChocolatey(); err != nil {
-		return err.Error()
-	}
-
-	return "Chocolatey installed: Setup completed"
-
 }
--- pkg/tools/downloadTools.go
+++ pkg/tools/downloadTools.go
@@ -12,22 +12,38 @@ import (
 
 func DownloadAndInstallTool(packageName string) (string, error) {
 
-	// Ensure chocolatey is installed on the system
-	if !IsChocolateyInstalled() {
-		if err := InstallChocolatey(); err != nil {
-			log.Fatal("Error installing chocolatey:", err)
+	// perform download operation based on the operating system
+	switch runtime.GOOS {
+	case "windows":
+		// Ensure chocolatey is installed on the system
+		if !IsChocolateyInstalled() {
+			if err := InstallChocolatey(); err != nil {
+				log.Fatal("Error installing chocolatey:", err)
+				return "", err
+			}
+		}
+
+		CheckChocolateyVersion()
+
+		// Install desired package using chocolatey
+		if err := chocoInstall(packageName); err != nil {
+			log.Fatal("Error installing package:", err)
 			return "", err
 		}
+		return "Package " + packageName + " is " + "installed successfully.", nil
+	case "linux":
+		if err := linuxInstall(packageName); err != nil {
+			return "", err
+		}
+		return "Package " + packageName + " is " + "installed successfully.", nil
+	case "darwin":
+		if err := brewInstall(packageName); err != nil {
+			return "", err
+		}
+		return "Package " + packageName + " is " + "installed successfully.", nil
+	default:
+		return "", fmt.Errorf("unsupported operating system")
 	}
-
-	CheckChocolateyVersion()
-
-	// Install desired package using chocolatey
-	if err := chocoInstall(packageName); err != nil {
-		log.Fatal("Error installing package:", err)
-		return "", err
-	}
-	return "Package " + packageName + " is " + "installed successfully.", nil
 }
 
 // check if chocolatey is installed on the system
@@ -96,7 +112,7 @@ func CheckChocolateyVersion() (string, error) {
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println("Error: Chocolatey not found. Check installation.")
-		return "", fmt.Errorf("chocolatey not found. Check installation.")
+		return "", fmt.Errorf("chocolatey not found. Check installation")
 	} else {
 		fmt.Println("Chocolatey installed successfully.")
 		return "Chocolatey installed successfully.", nil
@@ -116,5 +132,46 @@ func updatePath(newPath string) error {
 		}
 	}
 
+	return nil
+}
+
+func IsBrewInstalled() bool {
+	if runtime.GOOS != "darwin" {
+		fmt.Println("Brew is only supported on macOS")
+	}
+
+	// check if brew executable exist in PATH
+	cmd := exec.Command("brew", "--version")
+	cmd.Env = os.Environ() // Use current environment variables
+	cmd.Stdout = nil       // suppress stdout ouptput
+	cmd.Stderr = nil       // suppress stderr ouptput
+
+	err := cmd.Run()
+	return err == nil
+}
+
+func brewInstall(packageName string) error {
+	// Install desired package using brew
+	cmd := exec.Command("brew", "install", packageName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error installing package %s: %v", packageName, err)
+	}
+	return nil
+}
+
+func linuxInstall(packageName string) error {
+	// Install desired package using apt-get
+	cmd := exec.Command("sudo", "apt-get", "install", "-y", packageName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error installing package %s: %v", packageName, err)
+	}
 	return nil
 }
