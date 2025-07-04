e26238531ed7110db00e4a65f20281ace45c772a
scriptnsam <oluwafemisam40@gmail.com>
2025-07-04 10:24:32 +0100

    chore: bump version for alpha release

--- cmd/root.go
+++ cmd/root.go
@@ -29,7 +29,7 @@ var rootCmd = &cobra.Command{
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
 		if versionFlag {
-			fmt.Println("v1.3.10-alpha")
+			fmt.Println("v1.3.11-alpha.0")
 		} else if setupFlag {
 			resp := dependency.SetupChocolatey()
 			fmt.Println(resp)
